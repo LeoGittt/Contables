e9a09018c29ca408b02ea4f2d1f12c0b8c7521b2
leonel gonzalez <133721483+LeoGittt@users.noreply.github.com>
2025-05-14 20:45:38 -0300

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import  HeroSection  from "@/components/sections/hero-section";
 import { AboutSection } from "@/components/sections/about-section";
 import { ServicesSection } from "@/components/sections/services-section";
-import { TeamSection } from "@/components/sections/team-section";
+import  TeamSection  from "@/components/sections/team-section";
 import { ContactSection } from "@/components/sections/contact-section";
 import { Footer } from "@/components/layout/footer";
 import { Header } from "@/components/layout/header";
